60da51f8426a3cf1b7d83e9b7f6cdb65e8a2dae2
cchouffon <58453159+cchouffon@users.noreply.github.com>
2020-01-23 11:04:28 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+       
+  <title><?php echo "Assignment 6"; ?></title>
+  <meta charset="utf-8"> 
+</head>
+<body>
+<h1><?php echo "Hello, word!"; ?></h1>
+<div>Allow me to introduce myself ! My name is <?php echo "Mahoug Houffon"; ?></div>
+<div>I am <?php echo "32"; ?> years old and I am a student at Metropolitan Community College. </div>
+<div> I am currently studying<?php echo "Botany";?> and I hope graduate in <?php echo "2020"; ?> .</div>
+
+</body>
+</html>
